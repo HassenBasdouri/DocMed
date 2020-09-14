37f100e61371872a81159e8e53daa9cf94ba8c99
HassenBasdouri <hassen.basdouri1995@gmail.com>
2020-09-14 11:04:17 +0100

    test intergation gitlab & github

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- icon -->
+    <!-- icon  -->
     <link rel="icon" href="{{url('images/img/docmed_logo.ico')}}">
 </head>
 <body>
